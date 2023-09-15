0106a3fc0648e6bbddbdb27c82f206f85fd20042
MayurKad <133734979+MayurKad@users.noreply.github.com>
2023-09-15 16:26:19 +0530

    Add files via upload

---
+++ python_var_&_Dt.py
@@ -0,0 +1,11 @@
+# chearting a variable
+charather_name = "jhon"
+charather_age = "20"
+print("The hero is coming to save a " + charather_name + "," )
+print("The hero age is " + charather_age + ",")
+print("The kid name is " + charather_name)
+print("The mom was happy for " + charather_age)
+#data types 
+int()
+float()
+str()
