ce3c201815b024942d012e0e1b0cdbafa3227f48
MayurKad <133734979+MayurKad@users.noreply.github.com>
2023-09-15 16:31:57 +0530

    Add files via upload

---
+++ python_str.py
@@ -0,0 +1,38 @@
+# using variabls to strings
+hello = "welcom to python"
+print( hello ) 
+
+# concatenation 
+Hello = " welcom to "
+print( Hello + " python ")
+
+#phrase lower case 
+H1 = "COME TO LEARN"
+print(H1.lower())
+
+#phrase upper case 
+H2 = " come to learn"
+print(H2.upper())
+
+#phrase upper case true or false
+H2 = " come to learn"
+print(H2.isupper())
+
+H1 = "COME TO LEARN"
+print(H1.islower())
+
+#phrase converting upper or lower case true or false
+
+H3 = " come to learn"
+print(H3.upper().isupper())
+
+H4 = "COME TO LEARN"
+print(H4.lower().islower())
+
+#finding or lookating the location of word in membory
+H5  = "come to college"
+print(H5[0::6])
+
+#index peramert 
+H6 = "come to college"
+print(H6.index("m"))
