c6b873ef57e29c9d8038b1f3a6967559fa091efe
MayurKad <133734979+MayurKad@users.noreply.github.com>
2023-09-20 23:26:17 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,3 @@
+#Tuple is a same like a list only different is tuple element can't change
+anime = ('baki', 'naruto', 'darling', 'death note') 
+print(anime) #in a tuple we an loat the loation of the data anf find out hte whih data inputed
