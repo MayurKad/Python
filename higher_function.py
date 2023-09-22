b84fe7478438f4eb3530c54fa722e60e756efca0
MayurKad <133734979+MayurKad@users.noreply.github.com>
2023-09-23 01:06:00 +0530

    Add files via upload

---
+++ higher_function.py
@@ -0,0 +1,21 @@
+# This is look like a class fuction (def).
+def shy_hi():
+    print("Hello world")
+
+shy_hi
+ 
+# This (def) fuction can use like a class and we can put the input in (def).
+
+def shyhi():
+    nm = input("enter the name :")
+    print(nm)
+
+shyhi()
+
+# This code we are going to do sum rescearch appling peramete.
+# We can take input in the parameters.
+
+def fk(name, age):
+    print("Hello " + name + ", your age is " + age)
+
+fk(input ("Enter the name :") ,input("Enter ur age :"))
