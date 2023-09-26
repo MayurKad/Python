c10ad2b1010207d1ddcbeee8648cb66a26120e43
MayurKad <133734979+MayurKad@users.noreply.github.com>
2023-09-26 16:12:39 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,13 @@
+is_male = False
+is_tall = True
+
+if is_male and is_tall:
+    print("ur the male and tall")
+
+elif is_male and not(is_tall):
+    print("ur male not a tall")
+
+elif not(is_male) and is_tall:
+    print("ur a not male and ur tall")
+else:
+    print("ur not a male and tall")
